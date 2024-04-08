88e4048151e7ddb1bfb8e2f97e374714ffd5a471
Leonardo Moretti <leonardonabarro2003@gmail.com>
2024-04-08 15:21:47 -0300

    feat: add api data

--- src/components/HomeHero/index.js
+++ src/components/HomeHero/index.js
@@ -1,12 +1,17 @@
+import millify from 'millify'
+
 import { useGetCryptosQuery } from '../../services/cryptoApi'
 
 import { Wrapper, InfosContainer, Infos } from './styles'
 
 const HomeHero = () => {
   const { data, isFetching } = useGetCryptosQuery()
+  const globalState = data?.data?.stats
 
   console.log(data)
   console.log(isFetching)
+
+  if (isFetching) return <h3>Loading...</h3>
 
   return (
     <Wrapper>
@@ -14,23 +19,23 @@ const HomeHero = () => {
       <InfosContainer>
         <Infos>
           <p>Total Cryptocurrencies</p>
-          <h3>5</h3>
+          <h3>{globalState.total}</h3>
         </Infos>
         <Infos>
           <p>Total Exchanges</p>
-          <h3>5</h3>
+          <h3>{millify(globalState.totalExchanges)}</h3>
         </Infos>
         <Infos>
           <p>Total Market Cap</p>
-          <h3>5</h3>
+          <h3>{millify(globalState.totalMarketCap)}</h3>
         </Infos>
         <Infos>
           <p>Total 24h Volume</p>
-          <h3>5</h3>
+          <h3>{millify(globalState.total24hVolume)}</h3>
         </Infos>
         <Infos>
           <p>Total Markets</p>
-          <h3>5</h3>
+          <h3>{millify(globalState.totalMarkets)}</h3>
         </Infos>
       </InfosContainer>
     </Wrapper>
